fd294b6a92ee5f5425986e99d7703bfb617c1ca8
manna84 <mannasingh84@gmail.com>
2024-11-07 19:10:16 +0530

    Build: Service Grid created

--- src/app/work/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Work() {
-  return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Work
-    </main>
-  );
-}
---
+++ src/data/data.ts
@@ -0,0 +1,157 @@
+export interface ServiceCategory {
+  name: string;
+  icon?: string;
+}
+
+export const servicesList: ServiceCategory[] = [
+  { name: "Landing Page" },
+  { name: "E-Commerce" },
+  { name: "Settings" },
+  { name: "Marketplace" },
+  {
+    name: "CRM, CMS",
+  },
+  {
+    name: "Shopify Design & Dev",
+    icon: "/icons/shopify.svg",
+  },
+  {
+    name: "Wordpress Development",
+    icon: "/icons/wordpress.svg",
+  },
+  {
+    name: "Webflow Development",
+    icon: "/icons/webflow.svg",
+  },
+  {
+    name: "Flutter Development",
+    icon: "/icons/flutter.svg",
+  },
+  {
+    name: "React Development",
+    icon: "/icons/react.svg",
+  },
+  {
+    name: "Front-end Development",
+    icon: "/icons/frontend.svg",
+  },
+  {
+    name: "Back-end Development",
+    icon: "/icons/backend.svg",
+  },
+  {
+    name: "Custom Web Development",
+    icon: "/icons/custom-web.svg",
+  },
+  {
+    name: "Dashboard",
+  },
+  {
+    name: "Blockchain / Decentralized Platforms",
+  },
+  {
+    name: "SaaS",
+  },
+  {
+    name: "Web 3.0 Design",
+  },
+  {
+    name: "Product Design",
+  },
+];
+
+export const appServices: ServiceCategory[] = [
+  { name: "Wireframes" },
+  { name: "App Content Hierarchy" },
+  { name: "App Visual Design" },
+  { name: "User flows and journey" },
+  {
+    name: "iOS",
+    icon: "/icons/ios.svg",
+  },
+  {
+    name: "Android",
+    icon: "/icons/android.svg",
+  },
+  {
+    name: "Windows",
+    icon: "/icons/windows.svg",
+  },
+  { name: "Watch OS" },
+  { name: "Mac OS" },
+  {
+    name: "TV OS",
+    icon: "/icons/tv.svg",
+  },
+  {
+    name: "Mobile App",
+    icon: "/icons/mobile.svg",
+  },
+  { name: "Web App" },
+  { name: "Custom App" },
+  { name: "AppStore & Playstore screens" },
+  { name: "Prototyping" },
+  { name: "Onboarding" },
+  { name: "Conversion Mechanism" },
+  {
+    name: "User Experience",
+    icon: "/icons/user-experience.svg",
+  },
+  {
+    name: "Gamification & Retention",
+    icon: "/icons/gamification.svg",
+  },
+];
+
+export const brandingServices: ServiceCategory[] = [
+  {
+    name: "Logo Design",
+    icon: "/icons/logo-design.svg",
+  },
+  { name: "Visual identity" },
+  { name: "Brand identity" },
+  { name: "Voice and tone" },
+  {
+    name: "Brand strategy",
+    icon: "/icons/brand-strategy.svg",
+  },
+  { name: "Brandbook" },
+  { name: "Brand style guides" },
+  {
+    name: "Market research & positioning",
+    icon: "/icons/market-research.svg",
+  },
+  { name: "Investor Deck" },
+  { name: "Social Media Assets" },
+  {
+    name: "Video Production",
+    icon: "/icons/video-production.svg",
+  },
+  { name: "Product Demo Videos" },
+  { name: "Litepapers" },
+  { name: "Pitch Deck" },
+  { name: "Email Marketing" },
+];
+
+export const consultingServices: ServiceCategory[] = [
+  {
+    name: "AI-Powered Analytics",
+    icon: "/icons/ai-analytics.svg",
+  },
+  {
+    name: "Business Growth Strategy",
+    icon: "/icons/growth-strategy.svg",
+  },
+  {
+    name: "Blockchain & Smart Contracts",
+    icon: "/icons/blockchain.svg",
+  },
+  {
+    name: "Data-Driven Insights",
+    icon: "/icons/data-insights.svg",
+  },
+  {
+    name: "Workflow Automation Consulting",
+    icon: "/icons/workflow-automation.svg",
+  },
+];
